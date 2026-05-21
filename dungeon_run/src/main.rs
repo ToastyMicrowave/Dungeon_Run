42b077ae3f4a30efda1d42230e50cf68c995a405
Kavish Chawla <kavishchawla.alt@gmail.com>
2026-05-21 16:58:33 +0100

    Fully functional game logic with HUD, and sprite rendering for player, skeletons, and coins, as well as game over screen.

--- dungeon_run/src/main.rs
+++ dungeon_run/src/main.rs
@@ -4,23 +4,50 @@ use ::rand;
 use dungeon_core::*;
 
 const TILE_SIZE: f32 = 64.0;
+const HUD_HEIGHT: f32 = 40.0;
+
+struct SpriteRect {
+    x: f32,
+    y: f32,
+}
+
+const SPRITE_FLOOR: SpriteRect = SpriteRect { x: 16.0 * 7.0, y: 0.0 };
+const SPRITE_WALL: SpriteRect = SpriteRect { x: 0.0, y: 0.0 };
+const SPRITE_PLAYER: SpriteRect = SpriteRect { x: 16.0 * 4.0, y: 0.0 };
+const SPRITE_SKELETON: SpriteRect = SpriteRect { x: 16.0 * 6.0, y: 16.0 * 3.0};
+const SPRITE_COIN: SpriteRect = SpriteRect { x: 16.0 * 6.0, y: 16.0 * 8.0 };
 
 fn window_conf() -> Conf {
     Conf {
         window_title: "Dungeon Run".to_string(),
         window_width: GRID_WIDTH as i32 * TILE_SIZE as i32,
-        window_height: GRID_HEIGHT as i32 * TILE_SIZE as i32,
+        window_height: GRID_HEIGHT as i32 * TILE_SIZE as i32 + HUD_HEIGHT as i32,
         window_resizable: false,
+        ..Default::default()
+    }
+}
+
+fn sprite_params(sprite: SpriteRect) -> DrawTextureParams {
+    DrawTextureParams {
+        dest_size: Some(Vec2::new(TILE_SIZE, TILE_SIZE)),
+        source: Some(Rect::new(sprite.x, sprite.y, 16.0, 16.0)),
         ..Default::default()
     }
 }
 
 #[macroquad::main(window_conf)]
 async fn main() {
+    let tileset = load_texture("assets/Dungeon_Tileset.png").await.unwrap();
+    let charset = load_texture("assets/Dungeon_Character.png").await.unwrap();
+
+    tileset.set_filter(FilterMode::Nearest); // Prevent blurring when scaling
+    charset.set_filter(FilterMode::Nearest);
     let mut rng = rand::rng();
-    let mut state: Game = new_game(HARD, &mut rng);
+    let mut state: Game = new_game(MEDIUM, &mut rng);
+    
     loop {
         clear_background(BLACK);
+
 
         let input = if is_key_pressed(KeyCode::W) || is_key_pressed(KeyCode::Up) {
             Input::Up
@@ -33,30 +60,42 @@ async fn main() {
         } else {
             Input::None
         };
+        draw_text(&format!("Score: {}", state.score), 10.0, 25.0, 30.0, WHITE);
+        draw_text(&format!("Lives: {}", state.lives_left), (GRID_WIDTH as f32 * TILE_SIZE as f32) as f32 / 2.3, 25.0, 30.0, WHITE);
+        draw_text(&format!("Time: {}s", state.time_left.as_secs()), (GRID_WIDTH as f32 * TILE_SIZE as f32) - 150.0, 25.0, 30.0, WHITE);
 
         for (y, row) in state.grid.iter().enumerate() {
             for (x, tile) in row.iter().enumerate() {
-                let pos = (x as f32 * TILE_SIZE, y as f32 * TILE_SIZE);
+                let pos = (x as f32 * TILE_SIZE, y as f32 * TILE_SIZE + HUD_HEIGHT);
                 match tile {
-                    TileType::Wall => draw_rectangle(pos.0, pos.1, TILE_SIZE, TILE_SIZE, PURPLE),
-                    TileType::Floor => draw_rectangle(pos.0, pos.1, TILE_SIZE, TILE_SIZE, PINK),
+                    TileType::Wall => draw_texture_ex(&tileset, pos.0, pos.1, PURPLE, sprite_params(SPRITE_WALL)),
+                    TileType::Floor => draw_texture_ex(&tileset, pos.0, pos.1, PURPLE, sprite_params(SPRITE_FLOOR)),
                     TileType::Obstacle => draw_rectangle(pos.0, pos.1, TILE_SIZE, TILE_SIZE, BLACK),
                 }
                 if state.player_position == (x as u8, y as u8) {
-                    draw_rectangle(pos.0 + 16.0, pos.1 + 16.0, TILE_SIZE - 32.0, TILE_SIZE - 32.0, GREEN);
+                    draw_texture_ex(&charset, pos.0, pos.1, WHITE, sprite_params(SPRITE_PLAYER));
                 } else if state.skeleton_positions.contains(&(x as u8, y as u8)) {
-                    draw_rectangle(pos.0 + 16.0, pos.1 + 16.0, TILE_SIZE - 32.0, TILE_SIZE - 32.0, WHITE);
+                    draw_texture_ex(&charset, pos.0, pos.1, WHITE, sprite_params(SPRITE_SKELETON));
                 } else if state.coin_positions.contains(&(x as u8, y as u8)) {
-                    draw_rectangle(pos.0 + 16.0, pos.1 + 16.0, TILE_SIZE - 32.0, TILE_SIZE - 32.0, GOLD);
+                    draw_texture_ex(&tileset, pos.0, pos.1, WHITE, sprite_params(SPRITE_COIN));
                 }
             }
         }
-
+        let score = state.score;
         let delta = Duration::from_secs_f32(get_frame_time());
         if let Some(new_state) = tick(state, input, delta, &mut rng) {
             state = new_state;
         } else {
-            state = new_game(HARD, &mut rng);
+            loop {
+                clear_background(BLACK);
+                draw_text("Game Over!", (GRID_WIDTH as f32 * TILE_SIZE as f32) / 2.5, (GRID_HEIGHT as f32 * TILE_SIZE as f32) / 2.0, 50.0, RED);
+                draw_text(&format!("Final Score: {}", score), (GRID_WIDTH as f32 * TILE_SIZE as f32) / 2.8, (GRID_HEIGHT as f32 * TILE_SIZE as f32) / 1.8, 30.0, WHITE);
+                if is_key_pressed(KeyCode::Enter) {
+                    state = new_game(MEDIUM, &mut rng);
+                    break;
+                }
+                next_frame().await;
+            }
         }
 
         next_frame().await;
